94ca3f64ae620abc5f781e7350f29dd9e4bd89ce
Arthur Hoek <arthur@arthurhoek.nl>
2015-09-18 19:11:09 +0200

    Get history from healthkit

--- Gewicht Watch Extension/HistoryInterfaceController.swift
+++ Gewicht Watch Extension/HistoryInterfaceController.swift
@@ -8,15 +8,16 @@
 
 import WatchKit
 import Foundation
+import HealthKit
 
 
 class HistoryInterfaceController: WKInterfaceController {
 	
-	
+	let healthManager:HealthManager = HealthManager()
 	// Model
 	// TODO: get from healthkit
 	
-	var history = [90.8, 89.5, 70, 80, 80, 80, 80, 80, 80]
+	var history:[HKSample] = [];
 
 	// MARK: - Outlets
 	
@@ -25,15 +26,41 @@ class HistoryInterfaceController: WKInterfaceController {
 	
 	
 	func reloadTable() {
-		historyTable.setNumberOfRows(history.count, withRowType: "HistoryRow")
-		
-		var index = 0
-		for entry in history {
-			if let row = historyTable.rowControllerAtIndex(index) as? HistoryRow {
-				row.weightLabel.setText("\(entry)")
+		print("reloading...")
+		healthManager.readMostRecentWeight({ (results, error) -> Void in
+			if (error != nil) {
+				print("Error reading weight from HealthKit Store: \(error.localizedDescription)")
+				return;
 			}
-			index++
-		}
+			print("success loading weights \(results.count)")
+			self.history = results;
+			
+			self.historyTable.setNumberOfRows(self.history.count, withRowType: "HistoryRow")
+			
+			let formatter = NSDateFormatter()
+			//formatter.dateStyle = .MediumStyle
+			//formatter.timeStyle = .ShortStyle
+			
+			formatter.dateFormat = "d MMM H:mm";
+			
+			var index = 0
+			for entry in self.history {
+				let weight = entry as? HKQuantitySample;
+				let kilograms = weight?.quantity.doubleValueForUnit(HKUnit.gramUnitWithMetricPrefix(.Kilo))
+				let date = (weight?.startDate)!
+				let dateString = formatter.stringFromDate(date)
+				
+				
+				if let row = self.historyTable.rowControllerAtIndex(index) as? HistoryRow {
+					row.weightLabel.setText("\(kilograms!)")
+					print(dateString)
+					row.dateLabel.setText(dateString)
+				}
+				index++
+			}
+			
+			
+		});
 	}
 	
 	
